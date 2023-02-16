1995bae28609550e6d107836acd6b243600c9066
yahya <yahya.abad9@gmail.com>
2023-02-16 09:30:53 -0800

    first program prints to stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this program simply print a string to the stdout'
+ *
+ * Return: Success
+ */
+int	main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
