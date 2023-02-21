f03c64e51615d8aa0f115ca3bbab3fb07c528a7c
yahya <yahya.abad9@gmail.com>
2023-02-21 07:15:58 -0800

    solving it using only one function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,31 +1,20 @@
 #include "main.h"
 /**
- * _putstr - string to be printed
  *
- * Description: none
- *
- * Return: void.
- */
-void	_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-/**
- * main - Entry point
- *
- * Description: this program prints a simple string in stdout
+ * main - Print a string on stdout
  *
  * Return: 0.
  */
 int	main(void)
 {
-	_putstr("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
