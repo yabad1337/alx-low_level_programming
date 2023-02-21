3da7cee911acb2cc2b61a8c6070dbdcc55e5627d
yahya <yahya.abad9@gmail.com>
2023-02-21 07:22:02 -0800

    alphabet again

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - a..z
+ * Description: this function prints alphabet from 'a' to 'z'
+ * Return: none
+ */
+
+void	print_alphabet(void)
+{
+	int	i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+}
